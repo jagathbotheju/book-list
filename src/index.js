ce999c9814b3e0703bf684159709a76c6defdb2b
jagath <jagathbotheju@gmail.com>
2021-09-18 14:34:52 +0300

    remove App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import { books } from './books';
-import Book from './Book';
+import Book from './Book'
 
 function BookList() {
   return (
